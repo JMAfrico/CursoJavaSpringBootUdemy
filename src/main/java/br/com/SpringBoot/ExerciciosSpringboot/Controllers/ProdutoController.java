c96a38bfd89b71ef2e5a07bda1a82f3467a852e4
JMAfrico <joao_marcossilva@hotmail.com>
2021-08-11 17:08:31 -0300

    Consulta paginada

--- src/main/java/br/com/SpringBoot/ExerciciosSpringboot/Controllers/ProdutoController.java
+++ src/main/java/br/com/SpringBoot/ExerciciosSpringboot/Controllers/ProdutoController.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -47,9 +49,17 @@ public class ProdutoController {
 	
 	//---------LISTANDO TODOS OS PRODUTOS--------------//
 	
+	//Todos os produtos
 	@GetMapping("/listar")
 	public Iterable<Produto> ListarProduto() {
 		return produtorepository.findAll();
+	}
+	
+	//Todos os produtos limitados por quantidade e páginas
+	@GetMapping(path = "/listar/{numPagina}")
+	public Iterable<Produto> ListarProdutoPorPagina(@PathVariable int numPagina) {
+		Pageable page = PageRequest.of(numPagina, 5);
+		return produtorepository.findAll(page);
 	}
 	
 	//---------LISTANDO PRODUTOS POR ID--------------//
--- src/main/java/br/com/SpringBoot/ExerciciosSpringboot/Model/Repositories/ProdutoRepository.java
+++ src/main/java/br/com/SpringBoot/ExerciciosSpringboot/Model/Repositories/ProdutoRepository.java
@@ -1,10 +1,14 @@
 package br.com.SpringBoot.ExerciciosSpringboot.Model.Repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
+//import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import br.com.SpringBoot.ExerciciosSpringboot.Model.Entities.Produto;
 
-public interface ProdutoRepository extends CrudRepository<Produto, Integer>{
+	//interface que traz os comandos CRUDs básicos para o Controller
+	/*public interface ProdutoRepository extends CrudRepository<Produto, Integer>{
+	}
+	*/
+	//interface que traz os comandos CRUDs básicos e consultas paginadas(além de outras) para o Controller
+	public interface ProdutoRepository extends PagingAndSortingRepository<Produto, Integer>{
 
-	
-}
+		
+	}
